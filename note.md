0d48b24bb9e3238b604dd553f885569c26437ed5
Mohsen Hariri <mhariri68@gmail.com>
2022-04-08 12:39:50 -0400

    refine files

--- note.md
+++ note.md
@@ -1,1 +1,1 @@
-The initial commit is the first version of Thumbnail Preserving Encryption developed by [Kimia Tajik](kimiatajik@gmail.com).
+The initial commit is the first version of Thumbnail Preserving Encryption developed by [Kimia Tajik](mailto:kimiatajik@gmail.com).
---
+++ server/app/index.js
@@ -0,0 +1,41 @@
+import TPEncryption from "./tpencryption.js"
+
+import async from 'async'
+import fs from 'fs'
+
+const IMGS_PATH = "./imgs";
+const NUMBER_OF_ITERATION = 1;
+const BLOCK_SIZE = 50;
+const KEY = "DpdZPaQ[lkF";
+
+function start(){
+
+const ENCRYPTED_IMGS_PATH = IMGS_PATH + "/encrypted";
+if (!fs.existsSync(ENCRYPTED_IMGS_PATH)){
+    fs.mkdirSync(ENCRYPTED_IMGS_PATH);
+}
+
+fs.readdir(IMGS_PATH, (err, files) => {
+    let counter = 1;
+
+    async.filter(files, function(file, callback) {
+        let file_path = `${IMGS_PATH}/${file}`;
+        let result_file_path = `${ENCRYPTED_IMGS_PATH}/${file}`;
+        let t = new TPEncryption(file_path, KEY, function (err) {
+            if (!err) {
+                console.log(`image #${counter} => ${file_path}`);
+                t.encrypt(NUMBER_OF_ITERATION, BLOCK_SIZE, result_file_path, function (time) {
+                    counter += 1;
+                    console.log(`encrypted in ${time} ms.\n`);
+                    callback();
+                });
+            }
+        });
+    }, function(err, results) {
+        // results now equals an array of the existing files
+    });
+});
+
+}
+
+export default start
