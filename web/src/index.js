b96045f124959c2abe8c432506bb7b4b53fd78cb
Mohsen Hariri <mhariri68@gmail.com>
2022-05-15 17:53:02 -0400

    block and interation input

--- web/src/index.js
+++ web/src/index.js
@@ -8,19 +8,31 @@ const showImage = (id, src) => {
   imgDiv.style.display = 'block'
 }
 
-const NUMBER_OF_ITERATION = 10
-const BLOCK_SIZE = 10
-const KEY = 'V7a6kjqDeQUBNAev118sjOp3fbv_RMsHorWHkzuDCsM'
-// const KEY = 'DpdZPaQ[lkF'
+const iteration = document.getElementById('iteration')
+const block = document.getElementById('block')
+
+iteration.addEventListener('change', updateValue)
+block.addEventListener('change', updateValue)
+
+function updateValue(e) {
+  console.log(e.target.value)
+}
 
 const doEncrypt = async (src) => {
   try {
+    const NUMBER_OF_ITERATION = iteration.value || 1
+    const BLOCK_SIZE = block.value || 10
+    const KEY = 'V7a6kjqDeQUBNAev118sjOp3fbv_RMsHorWHkzuDCsM'
+
     let counter = 1
 
     const tpe = new TPEncryption(src, KEY)
     const dataImg = await Jimp.read(src)
 
     tpe.init(dataImg)
+    console.log('BLOCK_SIZE', BLOCK_SIZE)
+    console.log('NUMBER_OF_ITERATION', NUMBER_OF_ITERATION)
+
     await tpe.encrypt(NUMBER_OF_ITERATION, BLOCK_SIZE, (time) => {
       counter += 1
       console.log(`encrypted in ${time} ms.\n`)
